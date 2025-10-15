fd462ca35af8f7c1cdc592c32fb9cd0431e075d2
Andrey Egoshin <andrey.egoshin@ispringsolutions.com>
2025-10-15 23:01:52 +0300

    Выполнил первое задание

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T ~uint8 | ~uint16 | ~uint32 | ~uint64](number T) T {
+	var result T
+	pointerNumber := unsafe.Pointer(&number)
+	size := int(unsafe.Sizeof(number))
+
+	for i := 0; i < size; i++ {
+		*((*uint8)(unsafe.Add(unsafe.Pointer(&result), size-i-1))) = *(*uint8)(unsafe.Add(pointerNumber, i))
+	}
+	return result
 }
 
 func TestСonversion(t *testing.T) {
@@ -36,6 +44,41 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestСonversion64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
 		},
 	}
 
