bb902739e9184186eb192c68e8048c40c0197b5a
Andrey Egoshin <andrey.egoshin@ispringsolutions.com>
2025-10-28 23:08:25 +0300

    Реализовал пятое задание

--- homework/functions/homework_test.go
+++ homework/functions/homework_test.go
@@ -7,19 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Map(data []int, action func(int) int) []int {
-	// need to implement
-	return nil
+func Map[T any](data []T, action func(T) T) []T {
+	if data == nil {
+		return nil
+	}
+	result := make([]T, 0, len(data))
+	for _, item := range data {
+		result = append(result, action(item))
+	}
+	return result
 }
 
-func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+func Filter[T any](data []T, action func(T) bool) []T {
+	if data == nil {
+		return nil
+	}
+	result := make([]T, 0)
+	for _, item := range data {
+		if action(item) {
+			result = append(result, item)
+		}
+	}
+	return result
 }
 
-func Reduce(data []int, initial int, action func(int, int) int) int {
-	// need to implement
-	return 0
+func Reduce[T any](data []T, initial T, action func(T, T) T) T {
+	result := initial
+	for _, item := range data {
+		result = action(result, item)
+	}
+	return result
 }
 
 func TestMap(t *testing.T) {
