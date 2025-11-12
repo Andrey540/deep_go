b1055698d477e3d44f50193d79a0bc10ff5fc825
Andrey Egoshin <andrey.egoshin@ispringsolutions.com>
2025-11-12 22:29:22 +0300

    Реализовал девятое задание

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,50 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	var result []string
+
+	value := reflect.ValueOf(person)
+	typeOf := reflect.TypeOf(person)
+
+	for i := 0; i < value.NumField(); i++ {
+		field := typeOf.Field(i)
+		fieldValue := value.Field(i)
+
+		tag := field.Tag.Get("properties")
+		fieldName := tag
+		options := ""
+		if commaIndex := strings.Index(tag, ","); commaIndex > 0 {
+			fieldName = tag[:commaIndex]
+			options = tag[commaIndex+1:]
+		}
+
+		if options == "omitempty" {
+			if isEmptyValue(fieldValue) {
+				continue
+			}
+		}
+
+		result = append(result, fmt.Sprintf("%s=%v", fieldName, fieldValue))
+	}
+	return strings.Join(result, "\n")
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
 }
 
 func TestSerialization(t *testing.T) {
